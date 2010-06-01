75de748fe0dfb99306461d48eb3e8919f29aa283
junu.pv <junu.pv@01018527-62cb-052a-2c1b-4f6f124abcae>
2010-06-01 05:58:23 +0000

    Option check box, fixed few bugs

--- transliterator/transli.js
+++ transliterator/transli.js
@@ -5,191 +5,194 @@
  * License: GPLv3
  */
 var mlrules = {
-'([ക-ഹ])്ര്\\^':'$1ൃ',
-'ന്ന്j':'ഞ്ഞ്',
-'ന്ന്g':'ങ്ങ്',
-'റ്റ്h':'ത്',
-'ന്റ്h':'ന്ത്',
-'അa':'ആ',
-'അi':'ഐ',
-'അu':'ഔ',
-'ഇi':'ഈ',
-'([ക-ഹ])്r':'$1്ര്',
-'ക്h':'ഖ്',
-'ഗ്h':'ഘ്',
-'ൻg':'ങ്',
-'ച്h':'ഛ്',
-'ജ്h':'ഝ്',
-'ട്h':'ഠ്',
-'ഡ്h':'ഢ്',
-'ത്h':'ഥ്',
-'ദ്h':'ധ്',
-'പ്h':'ഫ്',
-'ബ്h':'ഭ്',
-'ംa':'മ',
-'ംA':'മാ',
-'ംi':'മി',
-'ംI':'മീ',
-'ംu':'മു',
-'ംU':'മൂ',
-'ംr':'മ്ര്',
-'ംe':'മെ',
-'ംE':'മേ',
-'ംo':'മൊ',
-'ംO':'മോ',
-'ംp':'മ്പ്',
-'ം~':'മ്',
-'സ്h':'ഷ്',
-'ശ്h':'ഴ്',
-'ൺ~':'ണ്',
-'ൺN':'ണ്ണ്',
-'ൺT':'ണ്ട്',
-'ൻ~':'ന്',
-'ൻk':'ങ്ക്',
-'ൻj':'ഞ്',
-'ൻn':'ന്ന്',
-'ൻd':'ന്ദ്',
-'ൻt':'ന്റ്',
-'ർ~':'ര്',
-'ർr':'ര്ര്',
-'ർy':'ര്യ്',
-'ൽ~':'ല്',
-'ൽl':'ല്ല്',
-'ൽp':'ല്പ്',
-'ൾ~':'ള്',
-'ൾL':'ള്ള്',
-'([ക-ഹ])a':'$1ാ',
-'([ക-ഹ])്a':'$1',
-'ൺa':'ണ',
-'ൻa':'ന',
-'ർa':'ര',
-'ൽa':'ല',
-'ൾa':'ള',
-'ൿa':'ക',
-'([ക-ഹ])്A':'$1ാ',
-'ൺA':'ണാ',
-'ൻA':'നാ',
-'ർA':'രാ',
-'ൽA':'ലാ',
-'ൾA':'ളാ',
-'ൿA':'കാ',
-'([ക-ഹ])്i':'$1ി',
-'ൺi':'ണി',
-'ൻi':'നി',
-'ർi':'രി',
-'ൽi':'ലി',
-'ൾi':'ളി',
-'ൿi':'കി',
-'([ക-ഹ])്I':'$1ീ',
-'ൺI':'ണീ',
-'ൻI':'നീ',
-'ർI':'രീ',
-'ൽI':'ലീ',
-'ൾI':'ളീ',
-'ൿI':'കീ',
-'([ക-ഹ])ിi':'$1ീ',
-'([ക-ഹ])െe':'$1ീ',
-'([ക-ഹ])്u':'$1ു',
-'ൺu':'ണു',
-'ൻu':'നു',
-'ർu':'രു',
-'ൽu':'ലു',
-'ൾu':'ളു',
-'ൿu':'കു',
-'([ക-ഹ])്U':'$1ൂ',
-'ൺU':'ണൂ',
-'ൻU':'നൂ',
-'ർU':'രൂ',
-'ൽU':'ലൂ',
-'ൾU':'ളൂ',
-'ൿU':'കൂ',
-'([ക-ഹ])ൊo':'$1ൂ',
-'([ക-ഹ])്e':'$1െ',
-'ൺe':'ണെ',
-'ൻe':'നെ',
-'ർe':'രെ',
-'ൽe':'ലെ',
-'ൾe':'ളെ',
-'ൿe':'കെ',
-'([ക-ഹ])്E':'$1േ',
-'ൺE':'ണേ',
-'ൻE':'നേ',
-'ർE':'രേ',
-'ൽE':'ലേ',
-'ൾE':'ളേ',
-'ൿE':'കേ',
-'([ക-ഹ])i':'$1ൈ',
-'([ക-ഹ])്o':'$1ൊ',
-'ൺo':'ണൊ',
-'ൻo':'നൊ',
-'ർo':'രൊ',
-'ൽo':'ലൊ',
-'ൾo':'ളൊ',
-'ൿo':'കൊ',
-'([ക-ഹ])്O':'$1ോ',
-'ൺO':'ണോ',
-'ൻO':'നോ',
-'ർO':'രോ',
-'ൽO':'ലോ',
-'ൾO':'ളോ',
-'ൿO':'കോ',
-'([ക-ഹ])u':'$1\u0d57',
-'([അ-്])m':'$1ം',
-'A':'ആ',
-'B':'ബ്ബ്',
-'C':'ച്ച്',
-'D':'ഡ്',
-'E':'ഏ',
-'F':'ഫ്',
-'G':'ഗ്ഗ്',
-'H':'ഃ',
-'I':'ഐ',
-'J':'ജ്ജ്',
-'K':'ക്ക്',
-'L':'ൾ',
-'M':'മ്മ്',
-'N':'ൺ',
-'O':'ഓ',
-'P':'പ്പ്',
-'Q':'ക്യ്',
-'R':'റ്',
-'S':'ശ്',
-'T':'ട്',
-'U':'ഊ',
-'V':'വ്വ്',
-'W':'വ്വ്',
-'X':'ക്ഷ്',
-'Y':'യ്യ്',
-'Z':'ശ്ശ്',
-'a':'അ',
-'b':'ബ്',
-'c':'ച്',
-'d':'ദ്',
-'e':'എ',
-'f':'ഫ്',
-'g':'ഗ്',
-'h':'ഹ്',
-'i':'ഇ',
-'j':'ജ്',
-'k':'ക്',
-'l':'ൽ',
-'m':'മ്',
-'n':'ൻ',
-'o':'ഒ',
-'p':'പ്',
-'q':'ക്ക്',
-'r':'ർ',
-'s':'സ്',
-'t':'റ്റ്',
-'u':'ഉ',
-'v':'വ്',
-'w':'വ്',
-'x':'ക്ഷ്',
-'y':'യ്',
-'z':'ശ്',
-'~':'്',
-'_':'\u200c'
+'^([ക-ഹ])്ര്\\^$':'$1ൃ',
+'^ന്ന്j$':'ഞ്ഞ്',
+'^ന്ന്g$':'ങ്ങ്',
+'^റ്റ്h$':'ത്',
+'^ന്റ്h$':'ന്ത്',
+'^അa$':'ആ',
+'^അi$':'ഐ',
+'^അu$':'ഔ',
+'^ഇi$':'ഈ',
+'^([ക-ഹ])്r$':'$1്ര്',
+'^ക്h$':'ഖ്',
+'^ഗ്h$':'ഘ്',
+'^ൻg$':'ങ്',
+'^ച്h$':'ഛ്',
+'^ജ്h$':'ഝ്',
+'^ട്h$':'ഠ്',
+'^ഡ്h$':'ഢ്',
+'^ത്h$':'ഥ്',
+'^ദ്h$':'ധ്',
+'^പ്h$':'ഫ്',
+'^ബ്h$':'ഭ്',
+'^ംa$':'മ',
+'^ംA$':'മാ',
+'^ംi$':'മി',
+'^ംI$':'മീ',
+'^ംu$':'മു',
+'^ംU$':'മൂ',
+'^ംr$':'മ്ര്',
+'^ംe$':'മെ',
+'^ംE$':'മേ',
+'^ംo$':'മൊ',
+'^ംO$':'മോ',
+'^ംm$':'മ്മ്',
+'^ംp$':'മ്പ്',
+'^ം~$':'മ്',
+'^സ്h$':'ഷ്',
+'^ശ്h$':'ഴ്',
+'^ൺ~$':'ണ്',
+'^ൺN$':'ണ്ണ്',
+'^ൺT$':'ണ്ട്',
+'^ൻ~$':'ന്',
+'^ൻk$':'ങ്ക്',
+'^ൻj$':'ഞ്',
+'^ൻn$':'ന്ന്',
+'^ൻd$':'ന്ദ്',
+'^ൻt$':'ന്റ്',
+'^ർ~$':'ര്',
+'^ർr$':'ര്ര്',
+'^ർy$':'ര്യ്',
+'^ൽ~$':'ല്',
+'^ൽl$':'ല്ല്',
+'^ൽp$':'ല്പ്',
+'^ൾ~$':'ള്',
+'^ൾL$':'ള്ള്',
+'^([ക-ഹ])a$':'$1ാ',
+'^([ക-ഹ])്a$':'$1',
+'^ൺa$':'ണ',
+'^ൻa$':'ന',
+'^ർa$':'ര',
+'^ൽa$':'ല',
+'^ൾa$':'ള',
+'^ൿa$':'ക',
+'^([ക-ഹ])്A$':'$1ാ',
+'^ൺA$':'ണാ',
+'^ൻA$':'നാ',
+'^ർA$':'രാ',
+'^ൽA$':'ലാ',
+'^ൾA$':'ളാ',
+'^ൿA$':'കാ',
+'^([ക-ഹ])്i$':'$1ി',
+'^ൺi$':'ണി',
+'^ൻi$':'നി',
+'^ർi$':'രി',
+'^ൽi$':'ലി',
+'^ൾi$':'ളി',
+'^ൿi$':'കി',
+'^([ക-ഹ])്I$':'$1ീ',
+'^ൺI$':'ണീ',
+'^ൻI$':'നീ',
+'^ർI$':'രീ',
+'^ൽI$':'ലീ',
+'^ൾI$':'ളീ',
+'^ൿI$':'കീ',
+'^([ക-ഹ])ിi$':'$1ീ',
+'^([ക-ഹ])െe$':'$1ീ',
+'^([ക-ഹ])്u$':'$1ു',
+'^ൺu$':'ണു',
+'^ൻu$':'നു',
+'^ർu$':'രു',
+'^ൽu$':'ലു',
+'^ൾu$':'ളു',
+'^ൿu$':'കു',
+'^([ക-ഹ])്U$':'$1ൂ',
+'^ൺU$':'ണൂ',
+'^ൻU$':'നൂ',
+'^ർU$':'രൂ',
+'^ൽU$':'ലൂ',
+'^ൾU$':'ളൂ',
+'^ൿU$':'കൂ',
+'^([ക-ഹ])ൊo$':'$1ൂ',
+'^([ക-ഹ])്e$':'$1െ',
+'^ൺe$':'ണെ',
+'^ൻe$':'നെ',
+'^ർe$':'രെ',
+'^ൽe$':'ലെ',
+'^ൾe$':'ളെ',
+'^ൿe$':'കെ',
+'^([ക-ഹ])്E$':'$1േ',
+'^ൺE$':'ണേ',
+'^ൻE$':'നേ',
+'^ർE$':'രേ',
+'^ൽE$':'ലേ',
+'^ൾE$':'ളേ',
+'^ൿE$':'കേ',
+'^([ക-ഹ])i$':'$1ൈ',
+'^([ക-ഹ])്o$':'$1ൊ',
+'^ൺo$':'ണൊ',
+'^ൻo$':'നൊ',
+'^ർo$':'രൊ',
+'^ൽo$':'ലൊ',
+'^ൾo$':'ളൊ',
+'^ൿo$':'കൊ',
+'^([ക-ഹ])്O$':'$1ോ',
+'^ൺO$':'ണോ',
+'^ൻO$':'നോ',
+'^ർO$':'രോ',
+'^ൽO$':'ലോ',
+'^ൾO$':'ളോ',
+'^ൿO$':'കോ',
+'^([ക-ഹ])u$':'$1\u0d57',
+'^([അ-്])m$':'$1ം',
+'^A$':'ആ',
+'^B$':'ബ്ബ്',
+'^C$':'ച്ച്',
+'^D$':'ഡ്',
+'^E$':'ഏ',
+'^F$':'ഫ്',
+'^G$':'ഗ്ഗ്',
+'^H$':'ഃ',
+'^I$':'ഐ',
+'^J$':'ജ്ജ്',
+'^K$':'ക്ക്',
+'^L$':'ൾ',
+'^M$':'മ്മ്',
+'^N$':'ൺ',
+'^O$':'ഓ',
+'^P$':'പ്പ്',
+'^Q$':'ക്യ്',
+'^R$':'റ്',
+'^S$':'ശ്',
+'^T$':'ട്',
+'^U$':'ഊ',
+'^V$':'വ്വ്',
+'^W$':'വ്വ്',
+'^X$':'ക്ഷ്',
+'^Y$':'യ്യ്',
+'^Z$':'ശ്ശ്',
+'^a$':'അ',
+'^b$':'ബ്',
+'^c$':'ച്',
+'^d$':'ദ്',
+'^e$':'എ',
+'^f$':'ഫ്',
+'^g$':'ഗ്',
+'^h$':'ഹ്',
+'^i$':'ഇ',
+'^j$':'ജ്',
+'^k$':'ക്',
+'^l$':'ൽ',
+'^m$':'മ്',
+'^n$':'ൻ',
+'^o$':'ഒ',
+'^p$':'പ്',
+'^q$':'ക്ക്',
+'^r$':'ർ',
+'^s$':'സ്',
+'^t$':'റ്റ്',
+'^u$':'ഉ',
+'^v$':'വ്',
+'^w$':'വ്',
+'^x$':'ക്ഷ്',
+'^y$':'യ്',
+'^z$':'ശ്',
+'^~$':'്',
+'^_$':'\u200c'
 };
+
+var trasliteration_fields = {};
 
 /**
  * from Alex who wrote comment for http://blog.vishalon.net/index.php/javascript-getting-and-setting-caret-position-in-textarea/
@@ -314,23 +317,91 @@ function tiKeyPressed(event) {
     var code = e.charCode || e.keyCode;
     // If this keystroke is a function key of any kind, do not filter it
     if (e.charCode == 0) return true;       // Function key (Firefox only)
-    if (e.ctrlKey || e.altKey) return true; // Ctrl or Alt held down
+    if (e.ctrlKey || e.altKey) // Ctrl or Alt held down
+	{
+		if (e.ctrlKey && (code==77 || code==109))
+		{
+			var checkbox = document.getElementById(this.id+'cb');
+			if(checkbox)
+			{
+				if(!checkbox.checked)
+				{
+					trasliteration_fields[this.id] = true;
+					checkbox.checked = true;
+				}
+				else
+				{
+					trasliteration_fields[this.id] = false;
+					checkbox.checked = false;
+				}
+				return false;
+			}
+		}
+		else return true;
+	}
     if (code < 32) return true;             // ASCII control character
-    var targetElement = document.getElementById((e.target || e.srcElement).id);
-    var c = String.fromCharCode(code);
-	var oldCaretPosition = GetCaretPosition(targetElement);
-	var lastSevenChars = getLastSixChars(targetElement.value, oldCaretPosition);
-	var transPair = trans(lastSevenChars+c);
-	var newText = replaceTransStringAtCaret(targetElement.value, transPair[0].length, transPair[1], oldCaretPosition);
-	targetElement.value = newText;
-	setCaretAtNewPosition(targetElement, transPair[0], transPair[1], oldCaretPosition);
-    return false;
+	if(trasliteration_fields[this.id])
+	{
+		var targetElement = document.getElementById((e.target || e.srcElement).id);
+		var c = String.fromCharCode(code);
+		var oldCaretPosition = GetCaretPosition(targetElement);
+		var lastSevenChars = getLastSixChars(targetElement.value, oldCaretPosition);
+		var transPair = trans(lastSevenChars+c);
+		var newText = replaceTransStringAtCaret(targetElement.value, transPair[0].length, transPair[1], oldCaretPosition);
+		targetElement.value = newText;
+		setCaretAtNewPosition(targetElement, transPair[0], transPair[1], oldCaretPosition);
+		return false;
+	}
+    return true;
 }
 function transliterate(id) {
 	var len = arguments.length;
 	for(var i=0;i<len; i++)
 	{
 		var element = document.getElementById(arguments[i]);
-		element.onkeypress = tiKeyPressed;
+		if(element)
+		{
+			trasliteration_fields[arguments[i]] = true;
+			element.onkeypress = tiKeyPressed;
+		}
+	}
+}
+
+function transOptionOnClick()
+{
+	if(this.checked)
+	{
+		trasliteration_fields[this.value] = true;
+		this.checked = true;
+	}
+	else
+	{
+		trasliteration_fields[this.value] = false;
+		this.checked = false;
+	}
+}
+// change this value to "after" or "before" to position transliteration option check box
+var TO_POSITION = "after";
+function addTransliterationOption()
+{
+	var len = arguments.length;
+	for(var i=0;i<len; i++)
+	{
+		var element = document.getElementById(arguments[i]);
+		if(element)
+		{
+			var checkbox = document.createElement('input');
+			checkbox.id = arguments[i]+'cb';
+			checkbox.type = 'checkbox';
+			checkbox.value = arguments[i];
+			checkbox.onclick = transOptionOnClick;
+			checkbox.checked = true;
+			var para = document.createElement('p');
+			para.appendChild(checkbox);
+			var text = document.createTextNode("മലയാളത്തിലെഴുതുക (Ctrl+M)");
+			para.appendChild(text);
+			if(TO_POSITION=="after") element.parentNode.insertBefore(para, element.nextSibling);
+			else if(TO_POSITION=="before") element.parentNode.insertBefore(para, element);
+		}
 	}
 }
