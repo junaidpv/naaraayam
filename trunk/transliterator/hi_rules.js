67e85ed61c9f7ee6c0ba89498e9c2d351a07612e
junu.pv <junu.pv@01018527-62cb-052a-2c1b-4f6f124abcae>
2010-06-06 16:21:35 +0000

    hindi rules

---
+++ trunk/transliterator/hi_rules.js
@@ -0,0 +1,30 @@
+﻿/**
+ * Naaraayam trasliteration regular expression rules table for Hindi
+ * @author Junaid P V
+ * @date 2010-06-05
+ */
+ // Normal rules
+var rules = {
+'^\u0907i$':'\u0908',
+'^A$':'\u0906',
+'^B$':'\u092c\u094d\u092c',
+'^C$':'\u0915\u094d\u0915',
+'^D$':'\u0921',
+'^E$':'\u0908',
+'^F$':'\u092b\u094d\u092b',
+'^G$':'\u0917\u094d\u0917',
+'^H$':'\u0903',
+'^I$':'\u0910',
+'^J$':'\u091c\u094d\u091c',
+'^K$':'\u0915\u094d\u0915',
+'^L$':'\u0933',
+'^M$':'\u092e\u094d\u092e',
+'^N$':'
+'^i$':'\u0907',
+'^a$':'\u0905',
+
+};
+// Memorised rules
+var memrules = {
+
+};
