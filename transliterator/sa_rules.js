f8ddbe858c96a9cf1c6f9b8cef43376579c994af
junu.pv <junu.pv@01018527-62cb-052a-2c1b-4f6f124abcae>
2010-10-12 09:28:43 +0000

    Starting

---
+++ transliterator/sa_rules.js
@@ -0,0 +1,12 @@
+﻿/**
+ * Trasliteration regular expression rules table for Sanskrit
+ * @author Junaid P V ([[user:Junaidpv]])
+ * @date 2010-10-12
+ * License: GPLv3, CC-BY-SA 3.0
+ */
+ // Normal rules
+ var rules = {
+};
+// Memorised rules
+var memrules = {
+};
