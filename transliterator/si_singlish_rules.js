7eaeb2c1ad2e4a44131b0ce9e636cd024f4ca0d8
junu.pv <junu.pv@01018527-62cb-052a-2c1b-4f6f124abcae>
2011-02-23 07:32:09 +0000

    starting transliteration scheme for Sinhala script

---
+++ transliterator/si_singlish_rules.js
@@ -0,0 +1,21 @@
+/**
+ * Transliteration regular expression rules table for Sinhala (Singlish)
+ * @author Junaid P V ([[user:Junaidpv]]) with help of Nishantha Anuruddha (si.wikipedia.org/wiki/user:බිඟුවා)
+ * @date 2011-02-23
+ * License: GPLv3
+ */
+ 
+ if(tr_si_singlish== undefined) var tr_si_singlish = {};
+else tr_si_singlish = {};
+tr_si_singlish.text = "InScript";
+tr_si_singlish.description = "Malayalam InScript";
+ // Normal rules
+tr_si_singlish.rules = [
+['අa', '', 'ආ'],
+['ඇa', '', 'ඈ'],
+['ඉi', '', 'ඊ'],
+
+['a', '', 'අ'],
+['A', '', 'ඇ'],
+['i', '', 'ඉ']
+];
